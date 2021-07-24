c3055a1c5b7411746c14005d48d3d61c9d9581a9
Brian Lipsett <lipsettbrian3@gmail.com>
2021-07-24 10:38:28 -0600

    vault controller

---
+++ Keepr/Controllers/VaultsController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Keepr.Controllers
+{
+
+  [ApiController]
+  [Route("api[controller]")]
+
+  public class VaultsController : ControllerBase
+  {
+
+  }
+}
